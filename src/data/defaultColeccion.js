8f2af1f58c94714bf3f893468a019c5b58f781c0
Soujiro17 <vicente_tomi@hotmail.com>
2023-07-20 17:38:24 -0400

    nota minima

--- src/data/defaultColeccion.js
+++ src/data/defaultColeccion.js
@@ -2,6 +2,7 @@ export default {
   nombre: "Nueva colección",
   mismoPorcentaje: false,
   notaMinimaAprobacion: 40,
+  notaMinima: 10,
   notaMaxima: 70,
   promedioParcial: 0,
   promedioFinal: 0,
